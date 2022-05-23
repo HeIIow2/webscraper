6dc3ec162b89d5949323a0c97c5f8e69edfbc891
HeIIow2 <Lars.Noack@outlook.de>
2022-05-23 19:08:40 +0200

    added the couning of keywords

--- refactor/analyzing/main.py
+++ refactor/analyzing/main.py
@@ -6,6 +6,11 @@ import read_data
 import extract_keywords
 import compute_edges
 
+import trend_analysis
+
+RUN_DATA_PREPARATION_FOR_CLUSTERING = False
+ANALYZE_TRENDS = False
+
 DATA_PATH = "data"
 ANALYZED_PATH = "analyzed"
 GRAPH_PATH = "graph_matricies"
@@ -22,46 +27,75 @@ MIN_TEXT_HEADER_RATIO = 2       # cars of the text / chars of the header
 KEYWORDS_PER_ARTICLE = 10        # The number of keywords to be extracted from each article
 
 SAVE_FREQUENCY = 10
-RESET_KEYWORDS = True
+RESET_KEYWORDS = False
 DATE_FORMAT = "%d.%m.%Y"
 
 
 if __name__ == "__main__":
-    if RESET_KEYWORDS:
-        with open(os.path.join(ANALYZED_PATH, "keywords.json"), "w", encoding="utf-8") as f:
-            json.dump([], f)
-
-    data_iter = iter(read_data.Data(MAGAZINES, DATA_PATH=DATA_PATH))
-    keyword_buffer = []
-
-    timeout = -1
-    for i, (date, data) in enumerate(data_iter):
-        print(f"-------------{date}--------------")
-        print("extracting keywords")
-        keywords = extract_keywords.get_keywords(magazines=MAGAZINES, data=data, keywords_per_article=KEYWORDS_PER_ARTICLE, header_importance_factor=HEADER_IMPORTANCE_FACTOR, min_text_header_ratio=MIN_TEXT_HEADER_RATIO)
-        for magazine in keywords:
-            print(magazine)
-            for key in keywords[magazine]:
-                #print("; ".join([element[0] for element in key]))
-                compute_edges.save_labels(magazine, key)
-            keyword_buffer.append({"date": datetime.strftime(date, DATE_FORMAT), "magazin": magazine, "keywords": keywords[magazine]})
-
-        if not i % SAVE_FREQUENCY:
-            print("saving keywords")
-            with open(os.path.join(ANALYZED_PATH, "keywords.json"), "r", encoding="utf-8") as f:
-                current_keywords = json.load(f)
-            current_keywords.extend(keyword_buffer)
+    if RUN_DATA_PREPARATION_FOR_CLUSTERING:
+        if RESET_KEYWORDS:
             with open(os.path.join(ANALYZED_PATH, "keywords.json"), "w", encoding="utf-8") as f:
-                json.dump(current_keywords, f)
+                json.dump([], f)
 
-        if i > timeout != -1:
-            break
+        data_iter = iter(read_data.Data(MAGAZINES, DATA_PATH=DATA_PATH))
+        keyword_buffer = []
 
-    print("saving keywords")
+        timeout = -1
+        for i, (date, data) in enumerate(data_iter):
+            print(f"-------------{date}--------------")
+            print("extracting keywords")
+            keywords = extract_keywords.get_keywords(magazines=MAGAZINES, data=data, keywords_per_article=KEYWORDS_PER_ARTICLE, header_importance_factor=HEADER_IMPORTANCE_FACTOR, min_text_header_ratio=MIN_TEXT_HEADER_RATIO)
+            for magazine in keywords:
+                print(magazine)
+                for key in keywords[magazine]:
+                    #print("; ".join([element[0] for element in key]))
+                    compute_edges.save_labels(magazine, key)
+                keyword_buffer.append({"date": datetime.strftime(date, DATE_FORMAT), "magazin": magazine, "keywords": keywords[magazine]})
+
+            if not i % SAVE_FREQUENCY:
+                print("saving keywords")
+                with open(os.path.join(ANALYZED_PATH, "keywords.json"), "r", encoding="utf-8") as f:
+                    current_keywords = json.load(f)
+                current_keywords.extend(keyword_buffer)
+                with open(os.path.join(ANALYZED_PATH, "keywords.json"), "w", encoding="utf-8") as f:
+                    json.dump(current_keywords, f)
+
+            if i > timeout != -1:
+                break
+
+        print("saving keywords")
+        with open(os.path.join(ANALYZED_PATH, "keywords.json"), "r", encoding="utf-8") as f:
+            current_keywords = json.load(f)
+        current_keywords.extend(keyword_buffer)
+        with open(os.path.join(ANALYZED_PATH, "keywords.json"), "w", encoding="utf-8") as f:
+            json.dump(current_keywords, f)
+
+        compute_edges.finished(GRAPH_PATH)
+        exit(0)
+
+    # trend analysis
+    print("starting trend analysis")
     with open(os.path.join(ANALYZED_PATH, "keywords.json"), "r", encoding="utf-8") as f:
-        current_keywords = json.load(f)
-    current_keywords.extend(keyword_buffer)
-    with open(os.path.join(ANALYZED_PATH, "keywords.json"), "w", encoding="utf-8") as f:
-        json.dump(current_keywords, f)
+        keywords = json.load(f)
 
-    compute_edges.finished(GRAPH_PATH)
+    # hier werden alle instanzen der Klasse Keyword (trend_analysis.py) gespeichert, die dem magazin zugeordnet werden
+    processing_map = {}
+
+    for magazine in MAGAZINES:
+        path = os.path.join(ANALYZED_PATH, f"{magazine}_modularity.csv")
+        if not os.path.exists(path):
+            continue
+        processing_map[magazine] = trend_analysis.Keywords(path, magazine)
+
+    for entry in keywords:
+        magazine = entry["magazin"]
+        if magazine not in processing_map:
+            continue
+        date = datetime.strptime(entry["date"], DATE_FORMAT)
+        keywords = entry["keywords"]
+
+        according_class = processing_map[magazine]
+        according_class.add_data(date, keywords)
+
+    for magazine in processing_map:
+        processing_map[magazine].retrieve_data()
